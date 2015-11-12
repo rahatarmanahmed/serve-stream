0114d6a7c8cbd877fb07566ab4613a5e6dee5292
Raymond Hammarling <raymond@hammarlings.se>
2015-11-12 22:38:58 +0100

    Implement functionality

--- src/index.js
+++ src/index.js
@@ -1,1 +1,25 @@
-// The entry point of all to come...
+import cp from 'child_process';
+
+import express from 'express';
+import morgan from 'morgan';
+import subarg from 'subarg';
+
+const args = subarg(process.argv.slice(2));
+
+const app = express();
+
+app.use(morgan());
+
+for(const sub of args._) {
+    const [path, cmd] = sub._;
+
+    app.get(path, (req, res) => {
+        const proc = cp.spawn('sh', ['-c', cmd]);
+
+        req.pipe(proc.stdin);
+        proc.stderr.pipe(process.stdout, { end: false });
+        proc.stdout.pipe(res);
+    });
+}
+
+app.listen(8080);
